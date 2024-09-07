650f60d3331fbbc03800b2c49639db65f0725f0d
Nikhil Chandrakar <nichandr@akamai.com>
2024-09-07 16:20:13 +0530

    Add frontend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 const { createSchema, completeSchema } = require("./types");
 const Todo = require("./db");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post(
   "/todo",
